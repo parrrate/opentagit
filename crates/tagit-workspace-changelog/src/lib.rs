d99a55bb9af5d7f46e3c957cc63055f49a0f0fba
parrrate <alisa@parrrate.com>
2025-11-05 21:24:13 +0000

    `tagit-workspace-changelog`

---
+++ crates/tagit-workspace-changelog/src/lib.rs
@@ -0,0 +1,21 @@
+//! Implementation for the `tagit changelog` command.
+
+use tagit_changelog::bump_one_changelog;
+use tagit_workspace::{WorkspaceEntry, with_workspace, with_workspaces};
+
+/// Update `CHANGELOG.md` on all members of the workspace.
+pub fn bump_changelog(dry_run: bool) -> anyhow::Result<()> {
+    with_workspaces(|workspace| {
+        with_workspace(
+            workspace,
+            dry_run,
+            false,
+            |WorkspaceEntry {
+                 version,
+                 root,
+                 tag_prefix,
+                 ..
+             }| bump_one_changelog(version.clone(), root, tag_prefix, dry_run),
+        )
+    })
+}
