1d7ebb7e1298269034b4a0e0bb04d0e4d9ac45f3
parrrate <alisa@parrrate.com>
2025-11-05 18:12:19 +0000

    `remote_to_path`

--- crates/tagit-sub-core/src/lib.rs
+++ crates/tagit-sub-core/src/lib.rs
@@ -1,6 +1,6 @@
 //! Core parts of `tagit sub`, which might influence other commands.
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 /// Root for `tagit`-specific files.
 pub const TAGIT_DIR: &str = ".tagit";
@@ -10,7 +10,7 @@ pub const TAGIT_ATTRIBUTES_FILE: &str = ".tagit/.gitattributes";
 pub const TAGIT_SUB_DIR: &str = ".tagit/sub";
 
 /// Remote name for a particular subpath.
-pub fn remote_name(path: &Path) -> anyhow::Result<String> {
+pub fn path_to_remote(path: &Path) -> anyhow::Result<String> {
     Ok(format!(
         "_tagit/_/{}",
         path.to_str()
@@ -18,4 +18,30 @@ pub fn remote_name(path: &Path) -> anyhow::Result<String> {
             .map_err(anyhow::Error::msg)?
             .replace('\\', "/"),
     ))
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("missing subtree: {0}")]
+pub struct MissingPath<'a>(&'a str);
+
+impl<'a> MissingPath<'a> {
+    pub fn path(&self) -> &'a str {
+        self.0
+    }
+}
+
+fn full_path(path: impl AsRef<Path>) -> PathBuf {
+    Path::new(TAGIT_SUB_DIR).join(path)
+}
+
+pub fn remote_to_path<'a>(remote: &'a str) -> Result<Option<&'a str>, MissingPath<'a>> {
+    if let Some(path) = remote.strip_prefix("_tagit/_/") {
+        if full_path(path).exists() {
+            Ok(Some(path))
+        } else {
+            Err(MissingPath(path))
+        }
+    } else {
+        Ok(None)
+    }
 }
