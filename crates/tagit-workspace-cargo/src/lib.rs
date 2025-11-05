e9799a0f3344b81da3e398f0acd5bbe47ec1c729
parrrate <alisa@parrrate.com>
2025-11-05 20:18:02 +0000

    `tagit-workspace-cargo`

---
+++ crates/tagit-workspace-cargo/src/lib.rs
@@ -0,0 +1,57 @@
+use std::{fmt::Display, path::Path};
+
+use anyhow::Context;
+use cargo::core::{Package, Workspace};
+use semver::Version;
+use tagit_cfg::TagitCfg;
+use tagit_workspace::{TagitPackage, TagitWorkspace};
+
+struct CargoPackage<'a>(&'a Package);
+
+impl Display for CargoPackage<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl TagitPackage for CargoPackage<'_> {
+    fn manifest_path(&self) -> &Path {
+        self.0.manifest_path()
+    }
+
+    fn cfg(&self) -> anyhow::Result<TagitCfg> {
+        tagit_cfg_cargo::parse(self.0.manifest())
+            .with_context(|| format!("invalid metadata for {self}"))
+    }
+
+    fn name(&self) -> &str {
+        self.0.name().as_str()
+    }
+
+    fn version(&self) -> &Version {
+        self.0.version()
+    }
+
+    fn root(&self) -> &Path {
+        self.0.root()
+    }
+}
+
+pub struct CargoWorkspace<'a, 'b>(&'a Workspace<'b>, Vec<CargoPackage<'a>>);
+
+impl<'a, 'b> CargoWorkspace<'a, 'b> {
+    pub fn new(workspace: &'a Workspace<'b>) -> Self {
+        let members = workspace.members().map(CargoPackage).collect();
+        Self(workspace, members)
+    }
+}
+
+impl TagitWorkspace for CargoWorkspace<'_, '_> {
+    fn members(&self) -> Vec<&dyn TagitPackage> {
+        self.1.iter().map(|p| p as _).collect()
+    }
+
+    fn root_manifest(&self) -> &Path {
+        self.0.root_manifest()
+    }
+}
