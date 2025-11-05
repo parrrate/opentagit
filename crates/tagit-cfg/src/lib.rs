3030b576b52e4311757531f9ed7760c131638417
parrrate <alisa@parrrate.com>
2025-11-05 17:11:56 +0000

    `tagit-cfg`

---
+++ crates/tagit-cfg/src/lib.rs
@@ -0,0 +1,13 @@
+//! Configuration for `tagit`.
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Default)]
+#[serde(deny_unknown_fields, default)]
+/// Configuration for a specific package, from tool metadata section of the manifest.
+pub struct TagitCfg {
+    /// Ignore this package entirely.
+    pub skip: bool,
+    /// Do not do semver-guided retagging.
+    pub skip_retag: bool,
+}
