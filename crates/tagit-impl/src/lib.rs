5abade456ea2e57c0638040b5f7d0d5d668dcffc
parrrate <alisa@parrrate.com>
2025-11-06 00:17:45 +0000

    `tagit-impl`

---
+++ crates/tagit-impl/src/lib.rs
@@ -0,0 +1,46 @@
+//! Implementation of `tagit` CLI.
+
+use clap_complete::{generate, shells::Bash};
+use clap_mangen::generate_to;
+use tagit_command::Command;
+
+/// Handle [`Command`].
+pub fn run(
+    tagit_package: &'static str,
+    tagit_version: &'static str,
+    command: Command,
+    to_command: impl FnOnce() -> clap::Command,
+) -> anyhow::Result<()> {
+    match command {
+        #[cfg(feature = "tag")]
+        Command::Tag {
+            dry_run,
+            no_retag,
+            total_order,
+        } => tagit_tag::tag(tagit_package, tagit_version, dry_run, no_retag, total_order),
+        #[cfg(feature = "sub")]
+        Command::Sub { command } => tagit_sub::sub(command.unwrap_or_default()),
+        #[cfg(feature = "changelog")]
+        Command::Changelog { dry_run } => tagit_workspace_changelog::bump_changelog(dry_run),
+        Command::Completions => {
+            generate(
+                Bash,
+                &mut to_command(),
+                env!("CARGO_PKG_NAME"),
+                &mut std::io::stdout(),
+            );
+            Ok(())
+        }
+        Command::Doc => {
+            std::fs::create_dir_all("target/man1")?;
+            generate_to(to_command(), "target/man1")?;
+            Ok(())
+        }
+        #[cfg(not(feature = "tag"))]
+        Command::Tag { .. } => anyhow::bail!("enable `tag` feature"),
+        #[cfg(not(feature = "sub"))]
+        Command::Sub { .. } => anyhow::bail!("enable `sub` feature"),
+        #[cfg(not(feature = "changelog"))]
+        Command::Changelog { .. } => anyhow::bail!("enable `changelog` feature"),
+    }
+}
