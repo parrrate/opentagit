a18c5bcc4ff9c51065cf9711048fcae77b2b36bb
parrrate <alisa@parrrate.com>
2026-02-22 02:32:02 +0000

    `cargo-metadata`

--- crates/tagit-cfg-cargo/src/lib.rs
+++ crates/tagit-cfg-cargo/src/lib.rs
@@ -1,19 +1,18 @@
 //! `tagit-cfg` parsing for `cargo`.
 
 use anyhow::Context;
-use cargo::core::Manifest;
+use cargo_metadata::Package;
 use tagit_cfg::TagitCfg;
-use toml::Value;
 
-fn get_value(manifest: &Manifest) -> Option<&Value> {
-    manifest.custom_metadata()?.as_table()?.get("tagit")
+fn get_value(manifest: &Package) -> Option<&serde_json::Value> {
+    manifest.metadata.as_object()?.get("tagit")
 }
 
 /// Parse `[package.metadata.tagit]`.
-pub fn parse(manifest: &Manifest) -> anyhow::Result<TagitCfg> {
+pub fn parse(manifest: &Package) -> anyhow::Result<TagitCfg> {
     Ok(get_value(manifest)
         .cloned()
-        .map(Value::try_into)
+        .map(serde_json::from_value)
         .transpose()
         .context("failed to parse [package.metadata.tagit]")?
         .unwrap_or_default())
--- crates/tagit-workspace-cargo/src/lib.rs
+++ crates/tagit-workspace-cargo/src/lib.rs
@@ -5,75 +5,86 @@ use std::{
 };
 
 use anyhow::Context;
-use cargo::{
-    GlobalContext,
-    core::{Package, Workspace},
-};
+use cargo_metadata::{Metadata, MetadataCommand, Package};
 use semver::Version;
 use tagit_cfg::TagitCfg;
 use tagit_core::{Tagit, out};
 use tagit_workspace::{TagitPackage, TagitWorkspace, TagitWorkspaceProvider};
 
-struct CargoPackage<'a>(&'a Package);
+struct CargoPackage<'a>(&'a Package, &'a Path);
 
 impl Display for CargoPackage<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
+        write!(f, "{}@{}", self.0.name, self.0.version)
     }
 }
 
 impl TagitPackage for CargoPackage<'_> {
     fn manifest_path(&self) -> &Path {
-        self.0.manifest_path()
+        self.0.manifest_path.as_std_path()
     }
 
     fn cfg(&self) -> anyhow::Result<TagitCfg> {
-        tagit_cfg_cargo::parse(self.0.manifest())
-            .with_context(|| format!("invalid metadata for {self}"))
+        tagit_cfg_cargo::parse(self.0).with_context(|| format!("invalid metadata for {self}"))
     }
 
     fn name(&self) -> &str {
-        self.0.name().as_str()
+        &self.0.name
     }
 
     fn version(&self) -> &Version {
-        self.0.version()
+        &self.0.version
     }
 
     fn root(&self) -> &Path {
-        self.0.root()
+        self.1
     }
 
     fn paths(&self) -> anyhow::Result<Vec<PathBuf>> {
         Ok(self
             .0
-            .targets()
+            .targets
             .iter()
-            .filter_map(|target| target.src_path().path())
+            .map(|target| target.src_path.as_std_path())
             .map(Path::to_owned)
             .collect::<BTreeSet<_>>()
             .into_iter()
-            .chain(std::iter::once(self.0.manifest_path().to_owned()))
+            .chain(std::iter::once(
+                self.0.manifest_path.as_std_path().to_owned(),
+            ))
             .collect())
     }
 }
 
-pub struct CargoWorkspace<'a, 'b>(&'a Workspace<'b>, Vec<CargoPackage<'a>>);
+pub struct CargoWorkspace<'a>(Vec<CargoPackage<'a>>, &'a Path);
 
-impl<'a, 'b> CargoWorkspace<'a, 'b> {
-    pub fn new(workspace: &'a Workspace<'b>) -> Self {
-        let members = workspace.members().map(CargoPackage).collect();
-        Self(workspace, members)
+impl<'a> CargoWorkspace<'a> {
+    pub fn new(workspace: &'a Metadata, root_manifest: &'a Path) -> anyhow::Result<Self> {
+        let members = workspace
+            .workspace_packages()
+            .into_iter()
+            .map(|package| {
+                Ok(CargoPackage(
+                    package,
+                    package
+                        .manifest_path
+                        .parent()
+                        .context("no parent directory for the manifest")?
+                        .as_std_path(),
+                ))
+            })
+            .collect::<anyhow::Result<_>>()?;
+        Ok(Self(members, root_manifest))
     }
 }
 
-impl TagitWorkspace for CargoWorkspace<'_, '_> {
+impl TagitWorkspace for CargoWorkspace<'_> {
     fn members(&self) -> Vec<&dyn TagitPackage> {
-        self.1.iter().map(|p| p as _).collect()
+        self.0.iter().map(|p| p as _).collect()
     }
 
     fn root_manifest(&self) -> &Path {
-        self.0.root_manifest()
+        self.1
     }
 }
 
@@ -85,8 +96,6 @@ impl TagitWorkspaceProvider for CargoProvider {
         &self,
         f: &mut dyn FnMut(&dyn TagitWorkspace) -> anyhow::Result<()>,
     ) -> anyhow::Result<()> {
-        let ctx = &GlobalContext::default()?;
-        out!("found cargo", "{}", ctx.cargo_exe()?.display());
         let root = Tagit::root()?;
         out!("found root", "{}", root.display());
         let path = root.join("Cargo.toml");
@@ -94,9 +103,9 @@ impl TagitWorkspaceProvider for CargoProvider {
             out!("doesn't exist", "{}", path.display());
             return Ok(());
         }
-        let workspace = Workspace::new(&path, ctx)?;
-        out!("found workspace", "{}", workspace.root().display());
-        f(&CargoWorkspace::new(&workspace))
+        let workspace: Metadata = MetadataCommand::new().exec()?;
+        out!("found workspace", "{}", workspace.workspace_root);
+        f(&CargoWorkspace::new(&workspace, &path)?)
     }
 }
 
